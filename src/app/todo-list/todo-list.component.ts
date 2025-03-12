48c5d5861be85aef398ec0c34581eb59ce6eff45
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-13 02:36:12 +0300

    Update todo-list.component.ts

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,81 +1,3 @@
-// import { CommonModule } from '@angular/common'
-// import { Component, OnInit } from '@angular/core'
-// import {
-//   FormBuilder,
-//   FormControl,
-//   FormGroup,
-//   FormsModule,
-//   ReactiveFormsModule,
-//   Validators,
-// } from '@angular/forms'
-// import { Observable } from 'rxjs'
-// import { Task } from '../state/list.reducer'
-// import { Store } from '@ngrx/store'
-// import {
-//   selectAllTodos,
-//   selectCompletedTodos,
-//   selectIncompleteTodos,
-// } from '../state/list.selector'
-// import {
-//   addTask,
-//   completeTask,
-//   removeTask,
-//   resetTasks,
-// } from '../state/list.actions'
-
-// @Component({
-//   selector: 'app-todo-list',
-//   imports: [ReactiveFormsModule, CommonModule, FormsModule, FormsModule],
-//   templateUrl: './todo-list.component.html',
-//   styleUrl: './todo-list.component.scss',
-// })
-// export class TodoListComponent implements OnInit {
-//   todoForm?: FormGroup
-//   todos$?: Observable<Task[]>
-//   completedTodos$?: Observable<Task[]>
-//   incompleteTodos$?: Observable<Task[]>
-
-//   constructor(private fb: FormBuilder, private store: Store) {
-//     this.todos$ = this.store.select(selectAllTodos)
-//     this.completedTodos$ = this.store.select(selectCompletedTodos)
-//     this.incompleteTodos$ = this.store.select(selectIncompleteTodos)
-//   }
-
-//   ngOnInit(): void {
-//     this.todoForm = this.fb.group({
-//       name: new FormControl('', [Validators.min(2), Validators.required]),
-//     })
-//   }
-//   onSubmit() {
-//     this.todoForm?.reset()
-//   }
-
-//   addTodo(): void {
-//     if (this.todoForm?.valid) {
-//       this.store.dispatch(
-//         addTask({
-//           task: {
-//             name: this.todoForm.value.name,
-//             complete: false,
-//           },
-//         }),
-//       )
-//       this.todoForm.reset()
-//     }
-//   }
-//   completeTodo(id: string): void {
-//     this.store.dispatch(completeTask({ id }))
-//   }
-
-//   removeTodo(id: string): void {
-//     this.store.dispatch(removeTask({ id }))
-//   }
-
-//   resetAllTodos(): void {
-//     this.store.dispatch(resetTasks())
-//   }
-// }
-
 
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
@@ -126,7 +48,7 @@ export class TodoListComponent implements OnInit {
     this.completedTodos$ = this.store.select(selectCompletedTodos);
     this.incompleteTodos$ = this.store.select(selectIncompleteTodos);
 
-    // تحميل المهام من localStorage عند تهيئة المكون
+   
     this.loadTodosFromStorage();
   }
 
@@ -139,27 +61,27 @@ export class TodoListComponent implements OnInit {
       };
 
       this.store.dispatch(addTask({ task: newTask }));
-      this.updateLocalStorage();  // تحديث localStorage بعد إضافة مهمة جديدة
+      this.updateLocalStorage();  
       this.todoForm.reset();
     }
   }
 
   completeTodo(id: string): void {
     this.store.dispatch(completeTask({ id }));
-    this.updateLocalStorage();  // تحديث localStorage بعد إتمام المهمة
+    this.updateLocalStorage();  
   }
 
   removeTodo(id: string): void {
     this.store.dispatch(removeTask({ id }));
-    this.updateLocalStorage();  // تحديث localStorage بعد إزالة المهمة
+    this.updateLocalStorage();  
   }
 
   resetAllTodos(): void {
     this.store.dispatch(resetTasks());
-    this.updateLocalStorage();  // تحديث localStorage بعد إعادة تعيين المهام
+    this.updateLocalStorage();  
   }
 
-  // تحديث localStorage بكل المهام الحالية
+  
   private updateLocalStorage(): void {
     if (typeof window !== 'undefined' && window.localStorage) {
       this.todos$?.subscribe(todos => {
@@ -168,7 +90,7 @@ export class TodoListComponent implements OnInit {
     }
   }
 
-  // تحميل المهام من localStorage إذا كانت موجودة
+  
   private loadTodosFromStorage(): void {
     if (typeof window !== 'undefined' && window.localStorage) {
       const savedTodos = localStorage.getItem('todos');
@@ -179,7 +101,7 @@ export class TodoListComponent implements OnInit {
     }
   }
 
-  // دالة لتوليد ID عشوائي للمهمة الجديدة
+  
   private generateId(): string {
     return Math.random().toString(36).substr(2, 9);
   }
